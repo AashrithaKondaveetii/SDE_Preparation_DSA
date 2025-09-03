21c4fae57eb2c79820af813cd0e55f741d1284ad
Aashritha Kondaveeti <146298266+AashrithaKondaveetii@users.noreply.github.com>
2025-09-02 21:39:22 -0700

    Time: 730 ms (25.52%), Space: 32.4 MB (10.97%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,10 +1,35 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
+        #Swapping Logic
+        # def swap(i, j):
+        #     temp = nums[i]
+        #     nums[i] = nums[j]
+        #     nums[j] = temp
+
+        # ---Selection Sort
+        # for i in range(0, len(nums) - 1):
+        #     min_idx = i
+        #     for j in range(i+1, len(nums)):
+        #         if (nums[j] < nums[min_idx]):
+        #             min_idx = j
+        #     if (min_idx != i):
+        #         swap(i, min_idx)
+        # return nums
+
+        # ---Bubble Sort---
+        # for gap in range(1, len(nums)):
+        #     swapped = False
+        #     for i in range(0, len(nums) - gap):
+        #         if (nums[i] > nums[i + 1]):
+        #             swap(i, i+1)
+        #             swapped = True
+        #     if (not swapped):
+        #         break
+        # return nums
 
         def merge(arr, L, M, R):
             left, right = arr[L : M+1], arr[M+1 : R+1]
             i, j, k = L, 0, 0
-
             while j < len(left) and k < len(right):
                 if left[j] <= right[k]:
                     nums[i] = left[j]
@@ -26,12 +51,12 @@ class Solution:
         def mergeSort(arr, l, r):
             if l == r:
                 return
-            
-            m = (l + r) // 2
+            m = l + (r - l) // 2
             mergeSort(arr, l, m)
-            mergeSort(arr, m+1,r)
+            mergeSort(arr, m+1, r)
             merge(arr, l, m, r)
             return
 
-        mergeSort(nums, 0, len(nums)-1)
+        mergeSort(nums, 0, len(nums) - 1)
         return nums
+
