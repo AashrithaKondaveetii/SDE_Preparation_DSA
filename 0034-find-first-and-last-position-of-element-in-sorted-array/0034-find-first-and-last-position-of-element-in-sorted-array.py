3bfee68b16c30337b8b2183c32cf8dbbe99fd3c0
Aashritha Kondaveeti <146298266+AashrithaKondaveetii@users.noreply.github.com>
2026-01-07 13:53:19 -0800

    Time: 0 ms (100%), Space: 20.6 MB (13.78%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -6,25 +6,25 @@ class Solution:
             return [-1, -1]
         return [lower, upper]
     
-
     def findBound(self, nums: List[int], target: int, isFirst: bool):
         l, r = 0, len(nums) - 1
         while l <= r:
-            mid = l + (r - l)//2
+            mid = l + (r-l) // 2
 
             if nums[mid] == target:
                 if isFirst:
-                    if mid == l or nums[mid - 1] < target:
-                        return mid 
+                    if mid == l or nums[mid-1] < target:
+                        return mid
                     else:
                         r = mid - 1
                 else:
-                    if mid == r or nums[mid + 1] > target:
+                    if mid == r or nums[mid+1] > target:
                         return mid
                     else:
                         l = mid + 1
+
             elif nums[mid] < target:
                 l = mid + 1
             else:
                 r = mid - 1
-        return - 1
+        return -1
